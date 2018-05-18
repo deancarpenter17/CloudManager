b532d12467db40b8ef955f78a791c4e56a08b5f1
= <=>
2018-05-17 23:40:19 -0500

    key-pair creation and email logic complete

--- backend/ec2_handler.py
+++ backend/ec2_handler.py
@@ -2,8 +2,18 @@
 
 import boto3
 import time
+import random
+import string
 from botocore.exceptions import ClientError
 from pprint import pprint
+import os
+
+# Email imports
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 from constants import *
 
@@ -41,6 +51,9 @@ class EC2ResourceHandler:
             print e
             raise e
 
+        # Initialized in create_instance()
+        self.instance = None
+
     def get_images(self):
         # For now, just return these 5 default images
         # TODO: update to choose any free tier ami
@@ -65,8 +78,8 @@ class EC2ResourceHandler:
                 GroupId=default_security_group_id,
                 IpPermissions=[
                     {'IpProtocol': 'tcp',
-                     'FromPort': 80,
-                     'ToPort': 80,
+                     'FromPort': 22,
+                     'ToPort': 22,
                      'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
                 ]
             )
@@ -80,7 +93,6 @@ class EC2ResourceHandler:
 
     # build the EC2 Instance
     def create_instance(self, image_name):
-
         if not image_name:
             print("AMI ID missing..Exiting")
             exit()
@@ -95,38 +107,122 @@ class EC2ResourceHandler:
 
         security_groups = self.get_security_groups()
 
+        # create a key-pair so the user can SSH into the instance
+        # Public key is saved on AWS, private key is returned here
+        key_pair_name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(7))
+        private_key = self.create_key_pair(key_pair_name)
+
         instances = self.ec2_resource.create_instances(
             ImageId=image_id,
             InstanceType=INSTANCE_TYPE,
+            KeyName=key_pair_name,
             MaxCount=1,
             MinCount=1,
             Monitoring={'Enabled': False},
             SecurityGroupIds=security_groups
         )
         # Grab the first instance in the list
-        instance = instances[0]
+        self.instance = instances[0]
 
-        instance_id = instance.instance_id
+        instance_id = self.instance.instance_id
         print("Instance ID: " + instance_id)
 
         print("Starting instance")
 
         # Public IP/DNS is only available when instance is running
-        instance.wait_until_running()
+        self.instance.wait_until_running()
 
         print("instance is running")
 
         # Reload the instance attributes so DNS/IP will appear
-        instance.load()
+        self.instance.load()
 
-        public_dns = instance.public_dns_name
-        public_ip = instance.public_ip_address
+        public_dns = self.instance.public_dns_name
+        public_ip = self.instance.public_ip_address
 
         print("Public DNS: " + public_dns)
         print("Public IP: " + public_ip)
 
-    def create_key_pair(self):
-        pass
+        # Finally, email the user the instance details
+        print("Emailing instance details...")
+        self.email_instance_details("user@gmail.com", private_key)
+
+    def create_key_pair(self, key_pair_name):
+        # TODO: allow the user to choose an existing key-pair or create a new one
+        # For now, create a new key-pair every time.
+        try:
+            key_pair = self.ec2_client.create_key_pair(KeyName=key_pair_name)
+        except ClientError as e:
+            pass
+        else:
+            return key_pair
+
+    """
+    Once an instance is running, this method emails the instance details to the user, then creates & attaches
+    the key_pair.pem file to the email so the user receives these details. It also attaches a custom SSH bash script
+    """
+    def email_instance_details(self, user_email_address, private_key):
+        # instance of MIMEMultipart
+        msg = MIMEMultipart()
+        msg['From'] = SERVER_EMAIL_ADDRESS
+        msg['To'] = user_email_address
+        msg['Subject'] = "EC2 Instance Details"
+
+        # Create body of the email
+        body = "Attached is the private key and an ssh script for the instance\n\n"
+        body += "Instance Details: \n"
+        body += "Availability Zone: " + self.instance.placement['AvailabilityZone'] + '\n'
+        body += "Instance ID: " + self.instance.instance_id + "\n"
+        body += "Instance Type: " + self.instance.instance_type + "\n"
+        body += "Public DNS: " + self.instance.public_dns_name + "\n"
+        body += "Public IP: " + self.instance.public_ip_address + "\n"
+        msg.attach(MIMEText(body, 'plain'))
+
+        key_pair_file_name = private_key['KeyName'] + ".pem"
+        k = MIMEBase('application', 'octet-stream')
+        s = MIMEBase('application', 'octet-stream')
+
+        # Next, build the .pem file and SSH script to be emailed to the user
+        key_pair_pem_file = open(key_pair_file_name, 'a+')
+        ssh_script = open('server.sh', 'a+')
+        ssh_body = "#!/bin/\n"
+        ssh_body += "chmod 400 " + key_pair_file_name + '\n'
+        ssh_body += "ssh -i " + key_pair_file_name + " -o ServerAliveInterval=60 " + "ubuntu@" + self.instance.public_dns_name
+        try:
+            key_pair_pem_file.write(private_key['KeyMaterial'])
+            ssh_script.write(ssh_body)
+            # To change the payload into encoded form
+            key_pair_pem_file.seek(0)
+            ssh_script.seek(0)
+            k.set_payload(key_pair_pem_file.read())
+            s.set_payload(ssh_script.read())
+            # clean up the server's directory
+            os.remove(key_pair_file_name)
+            os.remove("server.sh")
+        except IOError as e:
+            print("Error creating .pem file")
+            raise e
+        finally:
+            key_pair_pem_file.close()
+
+        # encode into base64
+        encoders.encode_base64(k)
+        encoders.encode_base64(s)
+        k.add_header('Content-Disposition', "attachment; filename= %s" % key_pair_file_name)
+        s.add_header('Content-Disposition', "attachment; filename= %s" % "server.sh")
+        # attach the instance 'p' to instance 'msg'
+        msg.attach(k)
+        msg.attach(s)
+
+        try:
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.ehlo()
+            server.login(SERVER_EMAIL_ADDRESS, SERVER_EMAIL_PASSWORD)
+            server.sendmail(msg['From'], msg['To'], msg.as_string())
+            server.close()
+        except Exception as e:
+            print e
+            raise e
 
 def main():
     ec2_handler = EC2ResourceHandler(DEFAULT_REGION)
